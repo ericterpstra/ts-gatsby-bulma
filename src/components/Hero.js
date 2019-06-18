fb0d4de08b2f46af0f645c6603d993af4ce94aa0
Eric Terpstra <eterps@gmail.com>
2019-06-17 20:29:31 -0400

    First draft

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,12 @@
 import React, { PureComponent } from 'react'
 import InterestForm from './InterestForm'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default class Hero extends PureComponent {
   render() {
     return (
-      <section className="hero-home hero hero-frenchcuff is-fullheight is-bold">
+      <section className="hero-home hero hero-gavel is-fullheight is-bold">
         <div className="hero-body">
           <div className="container">
             <div className="columns">
@@ -14,19 +16,44 @@ export default class Hero extends PureComponent {
                   <h2 className="subtitle">
                     Get help with your traffic violation.
                   </h2>
-                  <p>
-                    Here are some things you might not know about traffic
-                    tickets.
+                  <p className="hero-content">
+                    There are a lot of things to know about fighting traffic
+                    tickets in Iowa. Here are some things you might not know
+                    about traffic tickets:
                   </p>
-                  <ul>
-                    <li>Do this thing</li>
-                    <li>Then do this</li>
-                    <li>Don't worry, we got this!</li>
+                  <ul className="hero-list fa-ul">
+                    <li>
+                      <span className="fa-li">
+                        <FontAwesomeIcon icon={faCheckSquare} />
+                      </span>
+                      Do this thing
+                    </li>
+                    <li>
+                      <span className="fa-li">
+                        <FontAwesomeIcon icon={faCheckSquare} />
+                      </span>
+                      Then do this
+                    </li>
+                    <li>
+                      <span className="fa-li">
+                        <FontAwesomeIcon icon={faCheckSquare} />
+                      </span>
+                      Don't worry, we got this!
+                    </li>
                   </ul>
+                  <p className="hero-content">
+                    Fill out the form and an experienced lawyer will get back to
+                    you with some great info! If you have your citation number
+                    (e.g. A389455), that would help us serve you faster and more
+                    effectively!
+                  </p>
                 </div>
               </div>
 
               <div className="column is-half">
+                <h2 className="hero-right-column-title is-size-2 has-text-centered">
+                  Free Consultation!
+                </h2>
                 <InterestForm />
               </div>
             </div>
--- src/components/InterestForm.js
+++ src/components/InterestForm.js
@@ -1,60 +1,70 @@
 import React, { PureComponent } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelope,
+  faUser,
+  faPhone,
+  faEdit,
+} from '@fortawesome/free-solid-svg-icons'
 
 export default class InterestForm extends PureComponent {
   render() {
     return (
       <div className="interest-form-wrapper">
         <div className="field">
-          <div className="control">
+          <div className="control  has-icons-left">
             <input className="input is-medium" type="text" placeholder="Name" />
+            <span className="icon is-small is-left">
+              <FontAwesomeIcon icon={faUser} />
+            </span>
           </div>
         </div>
 
         <div className="field">
-          <div className="control">
+          <div className="control has-icons-left">
             <input
               className="input is-medium"
               type="email"
               placeholder="Email"
             />
+            <span className="icon is-small is-left">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
           </div>
         </div>
 
         <div className="field">
-          <div className="control">
+          <div className="control has-icons-left">
             <input
               className="input is-medium"
               type="text"
               placeholder="Phone Number"
             />
+            <span className="icon is-small is-left">
+              <FontAwesomeIcon icon={faPhone} />
+            </span>
           </div>
         </div>
 
         <div className="field">
-          <div className="control">
+          <div className="control has-icons-left">
             <input
               className="input is-medium"
               type="text"
               placeholder="Ticket Number"
             />
+            <span className="icon is-small is-left">
+              <FontAwesomeIcon icon={faEdit} />
+            </span>
           </div>
         </div>
 
-        <div className="field is-grouped">
-          <p className="control is-expanded">
-            <span className="select is-medium is-fullwidth">
-              <select>
-                <option>Best Time to Call...</option>
-                <option value="morning">Morning</option>
-                <option value="morning">Afternoon</option>
-                <option value="morning">Evening</option>
-                <option value="morning">Email Only, Please</option>
-              </select>
-            </span>
-          </p>
-          <p className="control">
-            <a className="button is-warning is-medium">Request A Call</a>
-          </p>
+        <div className="field is-pulled-right">
+          <div className="control is-expanded">
+            <button className="button is-warning is-medium">
+              Request A Call
+            </button>
+          </div>
         </div>
       </div>
     )
